1c6c44feb9c4ba30320ff7c2373799199684efce
Merrygold <merrymcx@gmail.com>
2022-06-19 21:03:55 +0100

    setup contract deployment and frontend

---
+++ scripts/deploy.js
@@ -0,0 +1,25 @@
+const main = async () => {
+    const [deployer] = await hre.ethers.getSigners();
+    const accountBalance = await deployer.getBalance();
+  
+    console.log("Deploying contracts with account: ", deployer.address);
+    console.log("Account balance: ", accountBalance.toString());
+  
+    const xContractFactory = await hre.ethers.getContractFactory("XPortal");
+    const xContract = await xContractFactory.deploy();
+    await xContract.deployed();
+  
+    console.log("XPortal address: ", xContract.address);
+  };
+  
+  const runMain = async () => {
+    try {
+      await main();
+      process.exit(0);
+    } catch (error) {
+      console.log(error);
+      process.exit(1);
+    }
+  };
+  
+  runMain();
