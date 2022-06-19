5875e5be4ea29d64207ff5c0d438baea47a466fc
Merrygold <merrymcx@gmail.com>
2022-06-19 22:23:56 +0100

    setup wallet check and account connection

--- waveportal-starter-project/src/App.js
+++ waveportal-starter-project/src/App.js
@@ -1,12 +1,43 @@
-import * as React from "react";
+import React, { useEffect, useState } from "react";
 import { ethers } from "ethers";
 import './App.css';
 
-export default function App() {
+  const App = () => {
+    /*
+  * Just a state variable we use to store our user's public wallet.
+  */
+  const [currentAccount, setCurrentAccount] = useState("");
+  const checkIfWalletIsConnected = async () => {
+    try {
+      const { ethereum } = window;
 
-  const wave = () => {
-    
+      if (!ethereum) {
+        console.log("Make sure you have metamask!");
+        return;
+      } else {
+        console.log("We have the ethereum object", ethereum);
+      }
+
+      /*
+      * Check if we're authorized to access the user's wallet
+      */
+      const accounts = await ethereum.request({ method: "eth_accounts" });
+
+      if (accounts.length !== 0) {
+        const account = accounts[0];
+        console.log("Found an authorized account:", account);
+        setCurrentAccount(account)
+      } else {
+        console.log("No authorized account found")
+      }
+    } catch (error) {
+      console.log(error);
+    }
   }
+
+    useEffect(() => {
+      checkIfWalletIsConnected();
+    }, [])
   
   return (
     <div className="mainContainer">
@@ -17,13 +48,15 @@ export default function App() {
         </div>
 
         <div className="bio">
-        I am farza and I worked on self-driving cars so that's pretty cool right? Connect your Ethereum wallet and wave at me!
+        Merrygold here, I DevOps and Web3. I'm exploring these days. Connect your eth wallet and wave at me. 😉
         </div>
 
-        <button className="waveButton" onClick={wave}>
+        <button className="waveButton" onClick={null}>
           Wave at Me
         </button>
       </div>
     </div>
   );
 }
+
+export default App
