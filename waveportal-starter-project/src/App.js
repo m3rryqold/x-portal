22a67c7cb243224bdd3514810cdd68ec48507659
Merrygold <merrymcx@gmail.com>
2022-06-19 22:26:17 +0100

    setup wallet connection

--- waveportal-starter-project/src/App.js
+++ waveportal-starter-project/src/App.js
@@ -1,12 +1,9 @@
 import React, { useEffect, useState } from "react";
-import { ethers } from "ethers";
-import './App.css';
+import "./App.css";
 
-  const App = () => {
-    /*
-  * Just a state variable we use to store our user's public wallet.
-  */
+const App = () => {
   const [currentAccount, setCurrentAccount] = useState("");
+
   const checkIfWalletIsConnected = async () => {
     try {
       const { ethereum } = window;
@@ -18,15 +15,12 @@ import './App.css';
         console.log("We have the ethereum object", ethereum);
       }
 
-      /*
-      * Check if we're authorized to access the user's wallet
-      */
       const accounts = await ethereum.request({ method: "eth_accounts" });
 
       if (accounts.length !== 0) {
         const account = accounts[0];
         console.log("Found an authorized account:", account);
-        setCurrentAccount(account)
+        setCurrentAccount(account);
       } else {
         console.log("No authorized account found")
       }
@@ -35,25 +29,54 @@ import './App.css';
     }
   }
 
-    useEffect(() => {
-      checkIfWalletIsConnected();
-    }, [])
-  
+  /**
+  * Implement your connectWallet method here
+  */
+  const connectWallet = async () => {
+    try {
+      const { ethereum } = window;
+
+      if (!ethereum) {
+        alert("Get MetaMask!");
+        return;
+      }
+
+      const accounts = await ethereum.request({ method: "eth_requestAccounts" });
+
+      console.log("Connected", accounts[0]);
+      setCurrentAccount(accounts[0]);
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  useEffect(() => {
+    checkIfWalletIsConnected();
+  }, [])
+
   return (
     <div className="mainContainer">
-
       <div className="dataContainer">
         <div className="header">
         👋 Hey there!
         </div>
 
         <div className="bio">
-        Merrygold here, I DevOps and Web3. I'm exploring these days. Connect your eth wallet and wave at me. 😉
+          I am farza and I worked on self-driving cars so that's pretty cool right? Connect your Ethereum wallet and wave at me!
         </div>
 
         <button className="waveButton" onClick={null}>
           Wave at Me
         </button>
+
+        {/*
+        * If there is no currentAccount render this button
+        */}
+        {!currentAccount && (
+          <button className="waveButton" onClick={connectWallet}>
+            Connect Wallet
+          </button>
+        )}
       </div>
     </div>
   );
